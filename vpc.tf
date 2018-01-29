ecc0eac105e8315e9c65d745f620886ebbd014d3
Justin Hopkins <hopkinsju@gmail.com>
2018-01-29 15:51:12 -0500

    update vpc config

--- vpc.tf
+++ vpc.tf
@@ -4,4 +4,6 @@ module "vpc" {
         # It's probably better to fork or clone this repo if you intend to use in production
         # so any future changes dont mess up your existing infrastructure.
         source = "github.com/hopkinsju/terraform-aws-vpc"
+        access_key = "${var.access_key}"
+        secret_key = "${var.secret_key}"
     }
